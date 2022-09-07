7ca0870c3c36fb9d79c5911db57427fd0916348d
mikeybs1987 <michaelbstearns@gmail.com>
2022-09-07 14:58:07 -0700

    Implemented CreateEmployee command.

--- src/timecardsystem/timecardservice/domain/commands.py
+++ src/timecardsystem/timecardservice/domain/commands.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict
 
 from timecardsystem.common.domain import commands
 from timecardsystem.common.domain import model as common_model
+from timecardsystem.timecardservice.domain import model
+
+
+@dataclass
+class CreateEmployee(commands.Command):
+    employee_id: common_model.EmployeeID
+    name: common_model.EmployeeName
 
 
 @dataclass
@@ -11,7 +18,7 @@ class CreateTimecard(commands.Command):
     timecard_id: common_model.TimecardID
     employee_id: common_model.EmployeeID
     week_ending_date: datetime
-    dates_and_hours: Dict[datetime, List[int]]
+    dates_and_hours: Dict[datetime, model.WorkDayHours]
 
 
 @dataclass
