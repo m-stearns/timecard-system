9cbaa037b3b06abae4f4273c9d78d59479991a41
mikeybs1987 <michaelbstearns@gmail.com>
2022-09-09 13:08:09 -0700

    Unit tests to test the unit_of_work module for saving and retrieving an Employee aggregate.

---
+++ tests/integration/test_unit_of_work.py
@@ -0,0 +1,48 @@
+from timecardsystem.timecardservice.adapters import odm
+from timecardsystem.timecardservice.services import unit_of_work
+from timecardsystem.timecardservice.domain import model
+from timecardsystem.common.domain import model as common_model
+
+
+def test_can_save_an_employee(mongodb_session_factory):
+    employee_id = common_model.EmployeeID("94687e57-7316-4b92-8e76-4ac5c5c07230")
+    employee_name = common_model.EmployeeName("Azure Diamond")
+    employee = model.Employee(
+        employee_id, employee_name
+    )
+    
+    test_unit_of_work = unit_of_work.MongoDBUnitOfWork(mongodb_session_factory)
+    with test_unit_of_work:
+        test_unit_of_work.employees.add(employee)
+        test_unit_of_work.commit()
+
+    session = mongodb_session_factory()
+    client = session.client
+    database = client[odm.DATABASE_NAME]
+    employees_collection = database[odm.EMPLOYEES_COLLECTION_NAME]
+    rows = employees_collection.find({"_id": employee_id.value})
+    doc = rows[0]
+    assert doc["_id"] == employee_id.value
+    assert doc["name"] == employee_name.value
+
+
+def test_can_retrieve_an_employee(mongodb_session_factory):
+    employee_id = common_model.EmployeeID("94687e57-7316-4b92-8e76-4ac5c5c07230")
+    employee_name = common_model.EmployeeName("Azure Diamond")
+    
+    session = mongodb_session_factory()
+    client = session.client
+    database = client[odm.DATABASE_NAME]
+    employees_collection = database[odm.EMPLOYEES_COLLECTION_NAME]
+    employees_collection.insert_one({
+        "_id": employee_id.value,
+        "name": employee_name.value
+    })
+
+    test_unit_of_work = unit_of_work.MongoDBUnitOfWork(mongodb_session_factory)
+    employee = None
+    with test_unit_of_work:
+        employee = test_unit_of_work.employees.get(employee_id)
+
+    assert employee.id == employee_id
+    assert employee.name == employee_name
