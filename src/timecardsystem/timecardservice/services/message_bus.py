1c87847f18933c9349ec6a46d134eba81ab075b7
mikeybs1987 <michaelbstearns@gmail.com>
2022-09-14 11:38:40 -0700

    message bus now conducts a check to see if the event is an internal or external event before calling the handler for the event.

--- src/timecardsystem/timecardservice/services/message_bus.py
+++ src/timecardsystem/timecardservice/services/message_bus.py
@@ -29,12 +29,14 @@ class MessageBus:
         self.queue.append(message)
         while self.queue:
             message = self.queue.pop(0)
+            print(message)
             if isinstance(message, commands.Command):
                 self.handle_command(message)
             elif isinstance(message, events.Event):
                 self.handle_event(message)
             else:
                 raise Exception(f"{message} is not an Event or Command!")
+            print(f"HANDLED: {message}")
 
     def handle_command(self, command: commands.Command):
         handler = self.command_handlers[type(command)]
@@ -43,10 +45,13 @@ class MessageBus:
             self.queue.extend(self.unit_of_work.collect_events())
 
     def handle_event(self, event: events.Event):
-        for handler in self.event_handlers[type(event)]:
-            handler(event)
-            if self.collect_side_effect_events:
-                self.queue.extend(self.unit_of_work.collect_events())
-        if self.publish_external_events:
+        if type(event) in self.event_handlers:
+            for handler in self.event_handlers[type(event)]:
+                handler(event)
+                if self.collect_side_effect_events:
+                    self.queue.extend(self.unit_of_work.collect_events())
+
+        if type(event) in self.external_event_handlers \
+                and self.publish_external_events:
             for handler in self.external_event_handlers[type(event)]:
                 handler(event)
--- tests/end_to_end/test_api.py
+++ tests/end_to_end/test_api.py
@@ -17,3 +17,61 @@ def test_post_to_create_employee(setup_and_destroy_mongodb_data):
         f"{api_url}/employees", json=payload
     )
     assert response.status_code == 201
+
+
+@pytest.mark.usefixtures("restart_timecardservice_api")
+def test_post_to_create_timecard(setup_and_destroy_mongodb_data):
+    api_url = config.get_api_url()
+    employee_id = "5dbf600d-305a-4f77-b2b8-51401f443597"
+    employee_name = "Azure Diamond"
+    payload = {
+        "employee_id": employee_id,
+        "name": employee_name
+    }
+
+    response = requests.post(
+        f"{api_url}/employees", json=payload
+    )
+    assert response.status_code == 201
+
+    timecard_id = "aaa6eaa1-3197-4b3e-9b52-c91c55b91956"
+    week_ending_date = "2022-08-12"
+    dates_and_hours_json = {
+        "2022-08-08": {
+            "work_hours": "8.0",
+            "sick_hours": "8.0",
+            "vacation_hours": "8.0",
+        },
+        "2022-08-09": {
+            "work_hours": "8.0",
+            "sick_hours": "8.0",
+            "vacation_hours": "8.0",
+        },
+        "2022-08-10": {
+            "work_hours": "8.0",
+            "sick_hours": "8.0",
+            "vacation_hours": "8.0",
+        },
+        "2022-08-11": {
+            "work_hours": "8.0",
+            "sick_hours": "8.0",
+            "vacation_hours": "8.0",
+        },
+        "2022-08-12": {
+            "work_hours": "8.0",
+            "sick_hours": "8.0",
+            "vacation_hours": "8.0",
+        },
+    }
+
+    payload = {
+        "timecard_id": timecard_id,
+        "employee_id": employee_id,
+        "week_ending_date": week_ending_date,
+        "dates_and_hours": dates_and_hours_json
+    }
+
+    response = requests.post(
+        f"{api_url}/timecards", json=payload
+    )
+    assert response.status_code == 201
