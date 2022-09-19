14bdb689882c18573f4ebb6e0350e049cc886a44
mikeybs1987 <michaelbstearns@gmail.com>
2022-09-19 12:15:55 -0700

    Fixed formatting to be in line with flake8 styling.

--- tests/integration/test_unit_of_work.py
+++ tests/integration/test_unit_of_work.py
@@ -1,16 +1,18 @@
-from timecardsystem.timecardservice.adapters import odm
-from timecardsystem.timecardservice.services import unit_of_work
-from timecardsystem.timecardservice.domain import model
 from timecardsystem.common.domain import model as common_model
+from timecardsystem.timecardservice.adapters import odm
+from timecardsystem.timecardservice.domain import model
+from timecardsystem.timecardservice.services import unit_of_work
 
 
 def test_can_save_an_employee(mongodb_session_factory):
-    employee_id = common_model.EmployeeID("94687e57-7316-4b92-8e76-4ac5c5c07230")
+    employee_id = common_model.EmployeeID(
+        "94687e57-7316-4b92-8e76-4ac5c5c07230"
+    )
     employee_name = common_model.EmployeeName("Azure Diamond")
     employee = model.Employee(
         employee_id, employee_name
     )
-    
+
     test_unit_of_work = unit_of_work.MongoDBUnitOfWork(mongodb_session_factory)
     with test_unit_of_work:
         test_unit_of_work.employees.add(employee)
@@ -27,9 +29,11 @@ def test_can_save_an_employee(mongodb_session_factory):
 
 
 def test_can_retrieve_an_employee(mongodb_session_factory):
-    employee_id = common_model.EmployeeID("94687e57-7316-4b92-8e76-4ac5c5c07230")
+    employee_id = common_model.EmployeeID(
+        "94687e57-7316-4b92-8e76-4ac5c5c07230"
+    )
     employee_name = common_model.EmployeeName("Azure Diamond")
-    
+
     session = mongodb_session_factory()
     client = session.client
     database = client[odm.DATABASE_NAME]
